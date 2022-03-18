f5a5c4b0d14a27e4ca29a3f1893f8e3a3f8ea2fc
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2022-03-17 23:32:08 -0700

    Add files via upload

---
+++ Learn/A Tour of Go/Flow Control Statements/for-continued.go
@@ -0,0 +1,16 @@
+/*
+For continued
+The init and post statements are optional.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	sum := 1
+	for ; sum < 1000; {
+		sum += sum
+	}
+	fmt.Println(sum)
+}
