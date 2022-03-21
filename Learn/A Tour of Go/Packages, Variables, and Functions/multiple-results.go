8e8c8318e4f2e31214d2559a59122cc20507321a
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2022-03-20 22:03:39 -0700

    Add multiple-results.go

---
+++ Learn/A Tour of Go/Packages, Variables, and Functions/multiple-results.go
@@ -0,0 +1,21 @@
+/*
+
+Multiple results
+A function can return any number of results.
+
+The swap function returns two strings.
+
+*/
+
+package main
+
+import "fmt"
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func main() {
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+}
